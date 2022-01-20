b443177c4a27109e8ac409d52851963df274fcdd
phanben110 <phanben110@gmail.com>
2022-01-20 17:34:42 +0700

    update code metric test

--- mainWeb.py
+++ mainWeb.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os 
 import cv2
 import tempfile
 from BEN_MattingMethod import UnetSegmentation as unet 
@@ -7,6 +8,8 @@ from BEN_MattingMethod import mediapipe
 from BEN_MattingMethod import modNet
 from BEN_MattingMethod import P3MNet 
 from BEN_footer import footer
+from evaluate import * 
+
 optionOutput = []
 
 #st.title('BACKGROUND MATTING')
@@ -122,7 +125,7 @@ if method != "None":
 
 option = st.sidebar.selectbox(
      'How would you like to do?',
-     ('Image Matting', 'Video Matting'))
+     ('Image Matting', 'Video Matting', 'Evaluation'))
 
 background = st.sidebar.file_uploader("Upload background")
 tbackground = tempfile.NamedTemporaryFile(delete=False) 
@@ -291,8 +294,122 @@ if st.sidebar.checkbox('I agree above setting'):
                 print ( "done1" )
      
      
+    elif option == "Evaluation": 
+        filePath = st.sidebar.selectbox('Which is dataset?',
+                                      ('Test dataset', 'Custom dataset'))
+        batchSize = st.sidebar.slider('Batch Size?', 0, 500, 25)
 
-    
+
+
+        p="/home/pcwork/ai/ftech/finger/handGestureWithMatting/evaluation/archive/Achieve"
+
+        if filePath == 'Test dataset': 
+            p="/home/pcwork/ai/ftech/finger/handGestureWithMatting/evaluation/archive/Achieve/image"
+            pathAlpha = "/home/pcwork/ai/ftech/finger/handGestureWithMatting/evaluation/archive/Achieve/alpha"
+            path = os.listdir(p)
+            path.sort() 
+        elif filePath == 'Custom dataset': 
+            p= '/home/pcwork/ai/ftech/finger/handGestureWithMatting/evaluation/archive/Achieve/imageTest2' 
+            pathAlpha = '/home/pcwork/ai/ftech/finger/handGestureWithMatting/evaluation/archive/Achieve/alphaTest2' 
+            path = os.listdir(p) 
+            path.sort() 
+
+
+        if st.sidebar.button("Run"):
+            model = chooseMethod(method,imgBackground)
+            count = 0 
+            sad = 0  
+            mse = 0 
+            mad = 0
+            for i, pathImg in enumerate(path): 
+                count +=1 
+
+                image = cv2.imread(f"{p}/{pathImg}")
+                alphaTruth = cv2.imread(f"{pathAlpha}/{pathImg[:-4]}.png",0)
+
+                source = image.copy()
+                alpha, matting = model.imageMatting(image)
+                source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)                       
+                matting = alphaTruth
+                
+                if method == "Mediapipe Matting" :
+                  #  alphaPredict = cv2.cvtColor(alpha, cv2.COLOR_RGB2GRAY) 
+                    matting = cv2.cvtColor(matting, cv2.COLOR_BGR2RGB)                       
+                    cv2.imwrite('alphaMediapipe.png', alpha) 
+                    alphaPredict = cv2.imread('alphaMediapipe.png',0) 
+
+                elif method == "ModNet": 
+                    alphaPredict = cv2.imread('alpha.png',0)
+                else:
+                    matting = cv2.cvtColor(matting, cv2.COLOR_BGR2RGB)                    
+                    alphaPredict = cv2.cvtColor(alpha, cv2.COLOR_RGB2GRAY) 
+
+
+                if len (optionOutput) == 3:
+                    i1 = optionOutput[0].index(" ")
+                    i2 = optionOutput[1].index(" ")
+                    i3 = optionOutput[2].index(" ")
+                    frame1 = optionOutput[0][i1+1:]
+                    frame2 = optionOutput[1][i2+1:]
+                    frame3 = optionOutput[2][i3+1:]
+                    if method == "P3MNet":
+                        stframe1.image(f"{frame1}.png")   
+                        stframe2.image(f"{frame2}.png")   
+                        stframe3.image(f"{frame3}.png")   
+                    else:
+                        stframe1.image(eval(frame1))      
+                        stframe2.image(eval(frame2))      
+                        stframe3.image(eval(frame3))      
+                
+                elif len ( optionOutput ) == 2 : 
+                    i1 = optionOutput[0].index(" ")
+                    i2 = optionOutput[1].index(" ")
+                    frame1 = optionOutput[0][i1+1:]
+                    frame2 = optionOutput[1][i2+1:]
+                    if method == "P3MNet":
+                        stframe1.image(f"{frame1}.png")   
+                        stframe2.image(f"{frame2}.png")   
+                    else:
+                        stframe1.image(eval(frame1))      
+                        stframe2.image(eval(frame2))      
+                elif len (optionOutput) == 1 :     
+                    i1 = optionOutput[0].index(" ")
+                    frame1 = optionOutput[0][i1+1:]
+                    if method == "P3MNet":
+                        stframe1.image(f"{frame1}.png")   
+                    else:
+                        stframe1.image(eval(frame1))      
+                else :
+                    if method == "P3MNet":
+                        stframe1.image("matting.png")   
+                    else:
+                        stframe1.image(matting)
+
+                sad_diff, mse_diff, mad_diff = calculate_sad_mse_mad_whole_img(alphaPredict/255, alphaTruth/255)
+                sad += sad_diff 
+                mse += mse_diff 
+                mad += mad_diff 
+                chart.add_rows(mad_diff)
+                print (sad_diff,mse_diff, mad_diff )
+                print (count)
+                if count == batchSize: 
+                    break 
+            sadMean = sad/count 
+            mseMean = mse/count 
+            madMean = mad/count
+            st.write(f"SAD Mean = {sadMean}, MSE Mean = {mseMean}, MAD Mean = {madMean}")
+            print (f"sad Mean = {sadMean}, mse Mean = {mseMean}, mad Mean = {madMean}")
+            print ( "done" )
+
+
+
+
+      
+      
+
+
+
+
                 #if len (optionOutput) == 3:
                 #    i1 = optionOutput[0].index(" ")
                 #    i2 = optionOutput[1].index(" ")
